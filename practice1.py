5d6577665ccfc203dcd9551f112d7cd11ab9f6a0
satakshi0510-jpg <satakshi0510@gmail.com>
2025-09-05 10:07:53 +0530

    Update practice1.py

--- practice1.py
+++ practice1.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+# This is a basic program to take user input and generate the output for name and age.
 name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 
@@ -6,3 +7,12 @@ current_year= datetime.now().year
 year_100= current_year + (100-age)
 
 print(f"Hello {name}! You will turn 100 years old in the year {year_100}.")
+
+#This is a program to reverse an array, array=[1,2,3,4,5].
+
+arr=[1,2,3,4,5]
+n=len(arr)
+
+for i in range(n//2):
+    arr[i], arr[n-i-1]= arr[n-i-1], arr[i]
+    print("Reversed array is: ", arr)
